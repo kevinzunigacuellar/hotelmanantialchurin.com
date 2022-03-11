9228a0d2aab841dbdf59afa4b395c9f6ab8dc9f3
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-10 22:30:19 -0500

    redesign initial commit

--- components/Subtitle/index.js
+++
@@ -1,3 +0,0 @@
-export default function Subtitle({ children }) {
-  return <h1 className='pb-4 text-xl font-semibold'>{children}</h1>
-}
--- components/Title/index.js
+++
@@ -1,3 +0,0 @@
-export default function Title({ children }) {
-  return <h1 className='pb-4 text-2xl font-semibold'>{children}</h1>
-}
