b4d94e3eca14287cdde86de07c39018dd4446e93
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-12 17:26:19 -0500

    add title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,3 +1,3 @@
 export default function Title({ title }: { title: string }) {
-  return <h1 className="text-3xl font-semibold text-gray-900 mb-4">{title}</h1>
+  return <h1 className="text-2xl font-semibold text-gray-900 mb-4">{title}</h1>
 }
