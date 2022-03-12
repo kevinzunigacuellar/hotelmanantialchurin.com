03405ba7ba22094e6eb9685405c8707bb739baa7
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-12 00:00:31 -0500

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+      </Head>
       <body className="bg-gray-100">
         <Main />
         <NextScript />
