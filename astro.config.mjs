bfecc3586ab2f611784c38d3c56e9555acd77680
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-12-04 02:35:36 -0500

    add sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
